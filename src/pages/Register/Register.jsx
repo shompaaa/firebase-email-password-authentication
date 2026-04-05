a7d2aa8f473dbd40785a65457172f94e5dc5127f
shompaaa <shompaakter334@gmail.com>
2026-04-05 17:51:17 +0600

    update user name

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -139,9 +139,6 @@ const Register = () => {
                     Accept Our Terms & Conditions 
                   </label>
                 </div>
-                <div>
-                  <a className="link link-hover">Forgot password?</a>
-                </div>
                 <button className="btn btn-neutral mt-4">Register</button>
               </fieldset>
               {success && (
